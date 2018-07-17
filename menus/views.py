d662bcd70719ed5aa6ac8c8422cb22080d00bfc8
ramazan47 <ramazan.suleymanli@gmail.com>
2018-07-17 18:57:59 +0400

    users exclude

--- menus/views.py
+++ menus/views.py
@@ -20,7 +20,7 @@ class UsersView(View):
     def get(self, request, *args, **kwargs):
         if not request.user.is_authenticated():
             return render(request, "home.html", {})
-        users = User.objects.all()
+        users = User.objects.all().exclude(username=request.user.username)
         return render(request, "menus/users.html", {'object_list': users})
 
 
