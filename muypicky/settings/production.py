0069a484bc719ea9576585c355a16d198a031887
ramazan199 <ramazan.suleymanli@gmail.com>
2018-07-18 22:16:00 +0400

    sasdadads

--- muypicky/settings/production.py
+++ muypicky/settings/production.py
@@ -37,7 +37,7 @@ MANAGERS = ADMINS
 SECRET_KEY = os.environ.get('SECRET_KEY,','j2b_z(*4w+#)t^nz3)0n3da(tcj&3##klo73m76(x7%3z)b%85n!')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['friendstodos.herokuapp.com','.friendstodos.com' ]
 
